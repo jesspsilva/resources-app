124dbd9d76d062b58b80d161997e7a125ead02a5
jesspsilva <jps936@hotmail.com>
2021-02-24 18:34:12 +0000

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul>
+    </ul>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            storedResources: [
+                {
+                    id: 'offcial-guide',
+                    title: 'Offcial Guide',
+                    description: 'The official Vue.js documentation',
+                    link: 'https://vuejs.org'
+                },
+                {
+                    id: 'google',
+                    title: 'Google',
+                    description: 'Learn to google...',
+                    link: 'https://google.pt'
+                }
+            ],
+        }
+    }
+}
+</script>
