85e416466454a459981af1cfba96ec63818924f3
jesspsilva <jps936@hotmail.com>
2021-02-25 19:04:29 +0000

    Handle dialog display

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+    <base-dialog v-if="inputIsInvalid" title="Invalid input" @close="confirmError">
+        <template #default>
+            <p>Unfortunately, at least one input value is invalid</p>
+            <p>Please, check all inputs</p>
+        </template>
+        <template #actions>
+            <base-button @click="confirmError">Okay</base-button>
+        </template>
+    </base-dialog>
     <base-card>
         <form @submit.prevent="submitData">
             <div class="form-control">
@@ -22,12 +31,29 @@
 
 <script>
 export default {
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
     methods: {
         submitData() {
             const enteredTitle = this.$refs.titleInput.value;
             const enteredDescription = this.$refs.descriptionText.value;
             const enteredLink = this.$refs.linkInput.value;
+
+            if(
+                enteredTitle.trim() === '' || 
+                enteredDescription.trim() === '' || 
+                enteredLink.trim() === '') {
+                    this.inputIsInvalid = true;
+                    return;
+            }
+
             this.addResource(enteredTitle, enteredDescription, enteredLink);
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
         }
     },
     inject: ['addResource']
